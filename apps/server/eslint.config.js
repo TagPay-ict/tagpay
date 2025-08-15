cf9a18c7e6a7ef583447cd39e5ca444b9c66e101
ralph dune <ralph@ralphs-MacBook-Pro.local>
2025-08-15 23:46:53 +0100

    Added turborepo as a build tool

---
+++ apps/server/eslint.config.js
@@ -0,0 +1,49 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsparser = require('@typescript-eslint/parser');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-require-imports': 'warn'
+    }
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.es2021
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      'prefer-const': 'error',
+      'no-var': 'error'
+    }
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**', '*.config.js', '*.config.ts', 'src/@types/**']
+  }
+];
